6f5a6b239fe60e4375e9b19967a439a5d3d3b5d3
ASnisarenko <ASnisarenko@priocom.com>
2024-03-08 16:31:45 +0200

    list was implemented

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,10 @@
 package hw04lrucache
 
+import (
+	"errors"
+	"fmt"
+)
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -17,10 +22,141 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	front  *ListItem
+	back   *ListItem
+	length int
 }
 
-func NewList() List {
+func NewList() *list {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) PushFront(item interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: item,
+		Next:  l.front,
+	}
+
+	if l.front == nil {
+		l.front = newItem
+		l.back = l.front
+	} else {
+		l.front.Prev = newItem
+		l.front = newItem
+	}
+
+	l.length++
+	return newItem
+}
+
+func (l *list) PushBack(item interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: item,
+		Prev:  l.back,
+	}
+	if l.back == nil {
+		l.back = newItem
+		l.front = l.back
+	} else {
+		l.back.Next = newItem
+		l.back = newItem
+	}
+
+	l.length++
+	return newItem
+}
+
+// потенциально опасная операция. в item может быть передан элемент от другого списка
+// но иначе - не будет О(1)
+func (l *list) Remove(item *ListItem) {
+	if item == nil {
+		return
+	}
+
+	switch item {
+	case l.front:
+		l.front = item.Next
+		item.Next.Prev = nil
+		item.Next = nil
+	case l.back:
+		l.back = item.Prev
+		item.Prev.Next = nil
+		item.Prev = nil
+	default:
+		item.Prev.Next = item.Next
+		item.Next.Prev = item.Prev
+	}
+
+	l.length--
+}
+
+// та же ситуация, что и с Remove()
+func (l *list) MoveToFront(item *ListItem) {
+	if item == nil {
+		return
+	}
+	l.Remove(item) // удалили элемент length уменьшилась на 1
+	l.front.Prev = item
+	item.Next = l.front
+	l.front = item
+	l.length++
+}
+
+// ищем элемент с указанным значением начиная от указанного
+func (l *list) SearchNext(startItem *ListItem, v interface{}) (*ListItem, error) {
+	if !l.checkItem(startItem) {
+		return nil, errors.New("данный элемент не принадлежит текущему списку")
+	}
+
+	for i := startItem; i != nil; i = i.Next {
+		if i.Value == v {
+			return i, nil
+		}
+	}
+
+	return nil, nil
+}
+
+// возвращаем первый найденный по содержимому элемент списка
+func (l *list) SearchFirst(v interface{}) *ListItem {
+	item, _ := l.SearchNext(l.front, v)
+	return item
+}
+
+// проверяем принадлежит ли указанный элемент списка текущему листу
+func (l *list) checkItem(item *ListItem) bool {
+	if item == nil {
+		return false
+	}
+
+	for i := l.Front(); i != nil; i = i.Next {
+		if i == item {
+			return true
+		}
+	}
+	return false
+}
+
+func (l list) String() string {
+	res := ""
+	for i := l.Front(); i != nil; i = i.Next {
+		if i == l.front {
+			res = fmt.Sprintf("%v", i.Value)
+		} else {
+			res = res + fmt.Sprintf(", %v", i.Value)
+		}
+	}
+	return res
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -1,6 +1,7 @@
 package hw04lrucache
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,9 +11,35 @@ func TestList(t *testing.T) {
 	t.Run("empty list", func(t *testing.T) {
 		l := NewList()
 
+		l.Remove(nil)
+		l.MoveToFront(nil)
+
 		require.Equal(t, 0, l.Len())
 		require.Nil(t, l.Front())
 		require.Nil(t, l.Back())
+	})
+
+	// table test
+	t.Run("stringer interface", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			items    []interface{}
+			expected string
+		}{
+			{"empty", []interface{}{}, ""},
+			{"strings", []interface{}{"Test test", "12345", "\n\t"}, "Test test, 12345, \n\t"},
+			{"multitype", []interface{}{"-128", 10, 11.5}, "-128, 10, 11.5"},
+		}
+		for _, tc := range tests {
+			tc := tc
+			t.Run(tc.name, func(t *testing.T) {
+				l := NewList()
+				for _, v := range tc.items {
+					l.PushBack(v)
+				}
+				require.Equal(t, tc.expected, fmt.Sprint(l))
+			})
+		}
 	})
 
 	t.Run("complex", func(t *testing.T) {
@@ -22,6 +49,7 @@ func TestList(t *testing.T) {
 		l.PushBack(20)  // [10, 20]
 		l.PushBack(30)  // [10, 20, 30]
 		require.Equal(t, 3, l.Len())
+		require.Equal(t, "10, 20, 30", l.String())
 
 		middle := l.Front().Next // 20
 		l.Remove(middle)         // [10, 30]
@@ -38,6 +66,7 @@ func TestList(t *testing.T) {
 		require.Equal(t, 7, l.Len())
 		require.Equal(t, 80, l.Front().Value)
 		require.Equal(t, 70, l.Back().Value)
+		require.Equal(t, "80, 60, 40, 10, 30, 50, 70", l.String())
 
 		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
 		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
@@ -48,4 +77,41 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("untyped list", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10)    // [10]
+		l.PushBack("test") // [10, 20]
+		l.PushBack(nil)    // [10, 20, 30]
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, "10, test, <nil>", l.String())
+
+		l2 := NewList()
+		l2.PushBack(10.45)
+		l.Back().Value = l2
+
+		require.Equal(t, "10, test, 10.45", l.String()) // l.String(...l2.String())
+
+	})
+
+	t.Run("search and move", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10)           // [10]
+		l.PushBack("test")        // [10, 20]
+		l.PushBack("second test") // [10, 20, 30]
+		l.PushBack(20)
+		l.PushBack(30)
+		require.Equal(t, 5, l.Len())
+		require.Equal(t, "10, test, second test, 20, 30", l.String())
+
+		l.MoveToFront(l.SearchFirst(30))
+		l.MoveToFront(l.SearchFirst(20))
+		l.MoveToFront(l.SearchFirst(10))
+
+		require.Equal(t, "10, 20, 30, test, second test", l.String())
+
+	})
+
 }
