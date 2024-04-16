22a6e327b7dbe54cc8201e760080ee4f7a33a537
Alexandr Snisarenko <asnisarenko@outlook.com>
2024-04-16 21:24:31 +0300

    hw07.02

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/schollz/progressbar/v3"
 )
@@ -14,26 +15,40 @@ var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 	ErrFileExists            = errors.New("destination file already exists")
+	ErrParams                = errors.New("not valid parameter")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64, rewrite bool) error {
+func checkCopyParams(fromPath, toPath string, offset, limit int64, rewrite bool) error {
 	var (
-		inFile, outFile         *os.File
 		inFileInfo, outFileInfo fs.FileInfo
 		err                     error
 	)
+
+	// базовые проверки значений параметров
+	switch {
+	case fromPath == "":
+		return fmt.Errorf("%w : name of file to read can't be empty", ErrParams)
+	case toPath == "":
+		return fmt.Errorf("%w : name of file to write can't be empty", ErrParams)
+	case limit < 0:
+		return fmt.Errorf("%w : limit can't be negative", ErrParams)
+	case offset < 0:
+		return fmt.Errorf("%w : offset can't be negative", ErrParams)
+	}
 
 	/// проверки параметров файлов ///
 	inFileInfo, err = os.Stat(fromPath)
 	if err != nil {
 		return fmt.Errorf("can't open file %s : %w", fromPath, err)
 	}
+
+	// обрабатываем только регулярные файлы
 	if mode := inFileInfo.Mode(); !mode.IsRegular() {
 		return ErrUnsupportedFile
 	}
 
-	// если свдиг больше файла - ошибка
-	if offset >= inFileInfo.Size() {
+	// если свдиг больше длины файла  - ошибка
+	if offset > inFileInfo.Size() {
 		return ErrOffsetExceedsFileSize
 	}
 
@@ -50,42 +65,78 @@ func Copy(fromPath, toPath string, offset, limit int64, rewrite bool) error {
 		}
 	}
 
-	/// обработка файлов ///
+	return nil
+}
+
+func Copy(fromPath, toPath string, offset, limit int64, rewrite bool) error {
+	var (
+		inFile, outFile *os.File
+		inFileInfo      fs.FileInfo
+		err             error
+	)
+
+	// проверяем корректность параметров и файлов
+	if err := checkCopyParams(fromPath, toPath, offset, limit, rewrite); err != nil {
+		return err
+	}
+
+	inFileInfo, err = os.Stat(fromPath)
+	if err != nil {
+		return fmt.Errorf("can't open file %s : %w", fromPath, err)
+	}
+
+	// если исходный файл нулевой длины или offset == file size - копировать нечего, создаём пустой выходнойфайл
+	if inFileInfo.Size() == 0 || inFileInfo.Size() == offset {
+		outFile, err = os.Create(toPath)
+		if err != nil {
+			return fmt.Errorf("can't create destination file: %w", err)
+		}
+		err = outFile.Close()
+		return err
+	}
+
+	// если данные для копирования есть - выполняем копирование
 	inFile, err = os.Open(fromPath)
 	if err != nil {
 		return fmt.Errorf("can't open source file: %w", err)
 	}
 	defer inFile.Close()
 
-	outFile, err = os.Create(toPath)
+	// пишем во временный файл. потом переименовываем
+	dir, file := filepath.Split(toPath)
+	outFile, err = os.CreateTemp(dir, file+".*.tmp")
 	if err != nil {
 		return fmt.Errorf("can't create destination file: %w", err)
 	}
 	defer outFile.Close()
 
-	fmt.Println("Copying ", inFile.Name(), " to ", outFile.Name())
+	fmt.Println("Copying ", fromPath, " to ", toPath)
 
 	// если задан сдвиг - сдвигаем ридер
 	if offset > 0 {
 		inFile.Seek(offset, io.SeekStart)
 	}
 
-	// если лимит не задан, или лимит, с учетом сдвига, больше размера файла, то limit (размер копируемых данных) = размер файла - сдвиг
+	// если лимит не задан, или лимит, с учетом сдвига, больше размера файла,
+	// то limit (размер копируемых данных) = размер файла - сдвиг
 	if limit == 0 || offset+limit > inFileInfo.Size() {
 		limit = inFileInfo.Size() - offset
 	}
 
+	// прогресс бар копирования
 	bar := progressbar.DefaultBytes(limit)
 
 	buf := make([]byte, 1024) //  буфер обмена
 	exit := false             // флаг завершения копирования
 	cntAll := int64(0)        // счетчик скопированных байт
+
+	// основной цикл копирования данных. выходим по лимиту или концу файла
 	for !exit {
 		cnt, err := inFile.Read(buf)
 		cntAll += int64(cnt)
 
 		if err != nil {
-			if err != io.EOF {
+			if errors.Is(err, io.EOF) {
 				return fmt.Errorf("error read from source file: %w", err)
 			}
 			exit = true
@@ -105,7 +156,10 @@ func Copy(fromPath, toPath string, offset, limit int64, rewrite bool) error {
 		bar.Add(cnt)
 	}
 
+	if err := os.Rename(outFile.Name(), toPath); err != nil {
+		return fmt.Errorf("error rename temporary file to destination file: %w", err)
+	}
+
 	fmt.Println("Copying completed")
 	return nil
-
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -6,38 +6,23 @@ import (
 )
 
 var (
-	from, to  string
-	limit, offset     int64
-	rewrite bool
+	vFrom, vTo      string
+	vLimit, vOffset int64
+	vRewrite        bool
 )
 
 func init() {
-	flag.StringVar(&from, "from", "", "file to read from")
-	flag.StringVar(&to, "to", "", "file to write to")
-	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
-	flag.Int64Var(&offset, "offset", 0, "offset in input file")
-	flag.BoolVar(&rewrite, "rewrite", false, "rewrite file if exists ")
+	flag.StringVar(&vFrom, "from", "", "file to read from")
+	flag.StringVar(&vTo, "to", "", "file to write to")
+	flag.Int64Var(&vLimit, "limit", 0, "limit of bytes to copy")
+	flag.Int64Var(&vOffset, "offset", 0, "offset in input file")
+	flag.BoolVar(&vRewrite, "rewrite", false, "rewrite file if exists ")
 }
 
 func main() {
 	flag.Parse()
-	// базовые проверки значений параметров
-	switch {
-	case from == "":
-		fmt.Println("Name of file to read can't be empty")
-		return
-	case to == "":
-		fmt.Println("Name of file to write can't be empty")
-		return
-	case limit < 0:
-		fmt.Println("Limit can't be negative")
-		return
-	case offset < 0:
-		fmt.Println("Offset can't be negative")
-		return
-	}
 
-	if err := Copy(from, to, offset, limit, rewrite); err != nil {
+	if err := Copy(vFrom, vTo, vOffset, vLimit, vRewrite); err != nil {
 		fmt.Println("Error:  ", err)
 	}
 }
