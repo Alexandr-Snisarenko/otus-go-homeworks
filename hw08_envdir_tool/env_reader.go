e07d5b72562d966d37fd3b90e8c4e06e751a16f8
ASnisarenko <ASnisarenko@priocom.com>
2024-05-09 13:06:59 +0300

    hw_08.01

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"os"
+	"strings"
+)
+
+var ErrEnvFileName = errors.New(" environment file name contains invalid characters: '=' ")
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +21,65 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment)
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return env, err
+	}
+
+	for _, file := range files {
+		// обрабатываем только регулярные файлы
+		if !file.Type().IsRegular() {
+			continue
+		}
+
+		if strings.Contains(file.Name(), "=") {
+			return env, ErrEnvFileName
+		}
+
+		envVal, err := ReadEnvFile(dir + "\\" + file.Name())
+		if err != nil {
+			return env, err
+		}
+
+		env[file.Name()] = envVal
+	}
+
+	return env, nil
+}
+
+func ReadEnvFile(fName string) (EnvValue, error) {
+	var (
+		env        EnvValue
+		needRemove bool //false
+		fLine      string
+	)
+
+	cFile, err := os.Open(fName)
+	if err != nil {
+		return env, err
+	}
+	defer cFile.Close()
+
+	fInfo, err := cFile.Stat()
+	if err != nil {
+		return env, err
+	}
+
+	// если размер 0 - ставим признак удаления переменной окружения
+	if fInfo.Size() == 0 {
+		needRemove = true
+	} else {
+		// если размер не 0 - читаем первую строку
+		fileScanner := bufio.NewScanner(cFile)
+		fileScanner.Scan()
+		fLine = fileScanner.Text()
+		// удаляем пробелі и табуляции
+		fLine = strings.TrimRight(fLine, "\t ")
+		// заменяем терминирующий 0x00 на перенос строки
+		fLine = string(bytes.Replace([]byte(fLine), []byte{0x00}[:], []byte("\n"), -1))
+	}
+
+	return EnvValue{fLine, needRemove}, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,68 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"strings"
+	"syscall"
+)
+
+var ErrOsEnvironmentVariableFormat = errors.New(" environment variable string don't contain '=' ")
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+func RunCmd(cmd []string, env Environment) error {
+	var (
+		newEnv []string
+		err    error
+	)
+	if newEnv, err = parseOsEnv(env); err != nil {
+		return err
+	}
+
+	err = syscall.Exec(cmd[0], cmd[1:], newEnv)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// функция парсировки текущего системного списка переменных окружения
+// и формирования нового списка переменных окружения
+// с учетом входного словаря переменных "на замену"
+// логика следующая:
+// 1 - переносим в новый список все переменные из системного списка, которых нет во входном словаре.
+// 2 - обрабатываем входной словарь и добавляем в новый список те переменные, у которых NeedRemove == false.
+func parseOsEnv(env Environment) ([]string, error) {
+	// получаем список текущих переменных окружения
+	osEnv := os.Environ()
+	// массив для новых переменных окружения (примерно такой же длинны)
+	newEnv := make([]string, 0, len(osEnv))
+
+	// проходим по текущему системному списку переменных окружения
+	for _, osEnv := range osEnv {
+		// определяем имя переменной по символу "="
+		// если "=" нет - unexpected error (такого быть не может по идее)
+		before, _, ok := strings.Cut(osEnv, "=")
+		if !ok {
+			return nil, ErrOsEnvironmentVariableFormat
+		}
+
+		// ищем переменную окружения в нашем словаре
+		// если нашли - пропускаем (они обработаются отдельным блоком)
+		// если не нашли - переносим в новый массив as is
+		if _, ok := env[before]; !ok {
+			newEnv = append(newEnv, osEnv)
+		}
+	}
+
+	// проходим по нашему словарю переменных окружения
+	// если признак NeedRemove == false - добавляем переменную в новый список
+	for eVal := range env {
+		if !env[eVal].NeedRemove {
+			newEnv = append(newEnv, eVal+"="+env[eVal].Value)
+		}
+	}
+
+	return newEnv, nil
 }
