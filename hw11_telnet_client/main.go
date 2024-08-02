dce449a84b0180c25ed8eff436bca0aa003ef650
Alexandr Snisarenko <asnisarenko@outlook.com>
2024-08-03 00:32:42 +0300

    hw11.02

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,56 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"fmt"
-	"io"
 	"log/slog"
-	"net"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/spf13/pflag"
 )
-
-func in2outRoutine(ctx context.Context, chIn <-chan string, wg *sync.WaitGroup, iOut io.Writer) error {
-	defer wg.Done()
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case text, ok := <-chIn:
-			if !ok {
-				return nil
-			}
-			_, err := iOut.Write([]byte(text))
-			if err != nil {
-				return fmt.Errorf("error write to Output: %w ", err)
-			}
-		}
-	}
-}
-
-func read2chan(iRead io.Reader) <-chan string {
-	out := make(chan string)
-	reader := bufio.NewReader(iRead)
-	go func() {
-		defer close(out)
-		for {
-			text, err := reader.ReadString('\n')
-			if err != nil {
-				if err.Error() == "EOF" {
-					fmt.Println("..EOF")
-					//					return
-				}
-				break
-			}
-			out <- text
-		}
-	}()
-	return out
-}
 
 func main() {
 	var timeout time.Duration
@@ -65,49 +23,28 @@ func main() {
 	pflag.DurationVar(&timeout, "timeout", 10, "timeout in second waiting for connection establishment")
 	pflag.Parse()
 
-	// аргументы командной строки (адрес и порт) должно быть 2. если не 2 - ошибка.
-	args := pflag.Args()
-	if len(args) != 2 {
-		fmt.Fprintf(os.Stderr, "Arguments count is wrong. Expected 2, recieved %d", len(args))
-		return
-	}
+	// // аргументы командной строки (адрес и порт) должно быть 2. если не 2 - ошибка.
+	// args := pflag.Args()
+	// if len(args) != 2 {
+	// 	fmt.Fprintf(os.Stderr, "Arguments count is wrong. Expected 2, recieved %d \n", len(args))
+	// 	return
+	// }
 
-	// подключение к серверу
-	addr := args[0] + ":" + args[1]
-	conn, err := net.DialTimeout("tcp", addr, timeout*time.Second)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot connect to host %s. Error: %s", addr, err.Error())
-		return
-	} else {
-		defer conn.Close()
-	}
+	// // подключение к серверу
+	// addr := args[0] + ":" + args[1]
+
+	addr := "localhost:4242"
+
+	t := NewTelnetClient(addr, timeout, os.Stdin, os.Stdout, os.Stderr)
 
 	// контекст с отменой.
-	ctx, cancel := context.WithCancel(context.Background())
+	// ctx, cancel := context.WithCancel(context.Background())
+	waitCh, err := t.Connect(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot connect to host %s. Error: %s\n", addr, err.Error())
+		return
+	}
 
-	chStdIn := read2chan(os.Stdin)
-	chConn := read2chan(conn)
+	<-waitCh
 
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	// горутиа чтения данных с сервера. читаем в stdout
-	go func() {
-		err := in2outRoutine(ctx, chConn, wg, os.Stdout)
-		if err != nil {
-			fmt.Fprint(os.Stderr, err.Error())
-		}
-		cancel()
-	}()
-
-	wg.Add(1)
-	// горутина записи данных в сервер. пишем из stdin
-	go func() {
-		err := in2outRoutine(ctx, chStdIn, wg, conn)
-		if err != nil {
-			fmt.Fprint(os.Stderr, err.Error())
-		}
-		cancel()
-	}()
-
-	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,21 +1,136 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"errors"
 	"io"
+	"net"
+	"sync"
 	"time"
 )
 
+var (
+	ErrConnectionAlreadyActive = errors.New("connection is already active")
+)
+
 type TelnetClient interface {
-	Connect() error
-	io.Closer
-	Send() error
-	Receive() error
+	Connect(context.Context) (<-chan struct{}, error)
+	Close() error
+	IsActive() bool
+	// Send() error
+	// Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+type telnetClient struct {
+	mu        sync.Mutex
+	active    bool
+	done      chan struct{}
+	address   string
+	timeout   time.Duration
+	inReader  io.ReadCloser
+	outWriter io.Writer
+	errWriter io.Writer
+	conn      net.Conn
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *telnetClient) Connect(ctx context.Context) (<-chan struct{}, error) {
+	if t.active {
+		return nil, ErrConnectionAlreadyActive
+	}
+
+	conn, err := net.DialTimeout("tcp", t.address, t.timeout*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	t.conn = conn
+	t.active = true
+	t.done = make(chan struct{})
+
+	go func() {
+		defer t.Close()
+		chIn := reader2chan(t.done, t.inReader, t.errWriter, t.Close)
+		chConn := reader2chan(t.done, t.conn, t.errWriter, t.Close)
+
+		chan2writer(t.done, chIn, t.conn, t.errWriter, t.Close)
+		chan2writer(t.done, chConn, t.outWriter, t.errWriter, t.Close)
+
+		for {
+			select {
+			case <-t.done:
+				return
+			case <-ctx.Done():
+				t.Close()
+				return
+			}
+		}
+
+	}()
+
+	return t.done, nil
+}
+
+func (t *telnetClient) Close() error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	if !t.active {
+		return nil
+	}
+	t.active = false
+	close(t.done)
+
+	return t.conn.Close()
+}
+
+func (t *telnetClient) IsActive() bool {
+	return t.active
+}
+
+func NewTelnetClient(address string, timeout time.Duration, inReader io.ReadCloser, outWriter io.Writer, errWriter io.Writer) TelnetClient {
+	return &telnetClient{active: false, address: address, timeout: timeout, inReader: inReader, outWriter: outWriter, errWriter: errWriter}
+}
+
+func chan2writer(done <-chan struct{}, chIn <-chan string, bufOut io.Writer, bufErr io.Writer, closeConn func() error) {
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case text, ok := <-chIn:
+				if !ok {
+					return
+				}
+				_, err := bufOut.Write([]byte(text))
+				if err != nil {
+					bufErr.Write([]byte(err.Error() + "\n"))
+					closeConn()
+					return
+				}
+			}
+		}
+	}()
+}
+
+func reader2chan(done <-chan struct{}, bufRead io.Reader, bufErr io.Writer, closeConn func() error) <-chan string {
+	out := make(chan string)
+	reader := bufio.NewReader(bufRead)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				text, err := reader.ReadString('\n')
+				if err != nil {
+					bufErr.Write([]byte(err.Error() + "\n"))
+					closeConn()
+					return
+				}
+				out <- text
+			}
+		}
+	}()
+	return out
+}
